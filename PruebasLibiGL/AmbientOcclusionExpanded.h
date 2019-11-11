404deb5b548f155364faa7e996eeb7335f491a39
Beñat Urtasun Marco <benaturtasun@hotmail.com>
2019-11-11 12:39:00 +0100

    Ambient occlusion sectorizado: ambient_occlusion_expanded_embree_sector, ambient_occlusion_expanded_impl_sectorized

--- PruebasLibiGL/AmbientOcclusionExpanded.h
+++ PruebasLibiGL/AmbientOcclusionExpanded.h
@@ -15,7 +15,11 @@
 #include "igl/embree/EmbreeIntersector.h"
 #include "igl/embree/../Hit.h"
 
-namespace AmbientOcclusionExpanded 
+using namespace std;
+using namespace Eigen;
+
+
+namespace AmbientOcclusionExpanded
 {
 
 	template <
@@ -52,46 +56,282 @@ namespace AmbientOcclusionExpanded
 			Eigen::PlainObjectBase<DerivedS> & S,
 			Eigen::PlainObjectBase< DerivedAOVect> &VO);
 
-	template void AmbientOcclusionExpanded::ambient_occlusion_expanded_embree<
-		Eigen::Matrix<double, -1, 3, 0, -1, 3>,
-		Eigen::Matrix<int, -1, 3, 0, -1, 3>,
-		Eigen::Matrix<double, -1, 3, 0, -1, 3>,
-		Eigen::Matrix<double, -1, 3, 0, -1, 3>,
-		Eigen::Matrix<double, -1, 1, 0, -1, 1>,
-		Eigen::Matrix<double, -1, 3, 0, -1, 3> >
-		(
-			Eigen::PlainObjectBase<Eigen::Matrix<double, -1, 3, 0, -1, 3> > const&,
-			Eigen::PlainObjectBase<Eigen::Matrix<int, -1, 3, 0, -1, 3> > const&,
-			Eigen::PlainObjectBase<Eigen::Matrix<double, -1, 3, 0, -1, 3> > const&,
-			Eigen::PlainObjectBase<Eigen::Matrix<double, -1, 3, 0, -1, 3> > const&, int,
-			Eigen::PlainObjectBase<Eigen::Matrix<double, -1, 1, 0, -1, 1> >&,
-			Eigen::PlainObjectBase<Eigen::Matrix<double, -1, 3, 0, -1, 3> >&);
+	template <
+		typename DerivedP,
+		typename DerivedN,
+		typename DerivedU,
+		typename DerivedSampl,
+		typename DerivedS,
+		typename DerivedAOVect>
+		IGL_INLINE bool ambient_occlusion_expanded_impl_sectorized(
+			const std::function<
+			bool(
+				const Eigen::Vector3f&,
+				const Eigen::Vector3f&)
+			> & shoot_ray,
+			const Eigen::PlainObjectBase<DerivedP> & P,
+			const Eigen::PlainObjectBase<DerivedN> & N,
+			const Eigen::PlainObjectBase<DerivedU> & U,//first line of face
+			const Eigen::PlainObjectBase<DerivedSampl> &samples, //n_samples X (m_sectors x 3)
+			Eigen::PlainObjectBase<DerivedS> & S, //ratio occlussion
+			Eigen::PlainObjectBase<DerivedAOVect> & VO);//non occluded averaged direction
 
-	template void  AmbientOcclusionExpanded::ambient_occlusion_expanded_embree<
-		Eigen::Matrix<double, -1, -1, 0, -1, -1>,
-		Eigen::Matrix<int, -1, -1, 0, -1, -1>,
-		Eigen::Matrix<double, -1, -1, 0, -1, -1>,
-		Eigen::Matrix<double, -1, -1, 0, -1, -1>,
-		Eigen::Matrix<double, -1, 1, 0, -1, 1>,
-		Eigen::Matrix<double, -1, -1, 0, -1, -1> >
-		(
-			Eigen::PlainObjectBase<Eigen::Matrix<double, -1, -1, 0, -1, -1> > const &,
-			Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1> > const &,
-			Eigen::PlainObjectBase<Eigen::Matrix<double, -1, -1, 0, -1, -1> > const &,
-			Eigen::PlainObjectBase<Eigen::Matrix<double, -1, -1, 0, -1, -1> > const &,
-			int,
-			Eigen::PlainObjectBase<Eigen::Matrix<double, -1, 1, 0, -1, 1> > &,
-			Eigen::PlainObjectBase<Eigen::Matrix<double, -1, -1, 0, -1, -1> > &
-		);
 
-	//template void igl::ambient_occlusion<
-	//	Eigen::Matrix<double, -1, -1, 0, -1, -1>, 
-	//	Eigen::Matrix<double, -1, -1, 0, -1, -1>, 
-	//	Eigen::Matrix<double, -1, 1, 0, -1, 1> >
-	//	(
-	//		std::function<bool(Eigen::Matrix<float, 3, 1, 0, 3, 1> const&, Eigen::Matrix<float, 3, 1, 0, 3, 1> const&)> const&, 
-	//		Eigen::PlainObjectBase<Eigen::Matrix<double, -1, -1, 0, -1, -1> > const&, 
-	//		Eigen::PlainObjectBase<Eigen::Matrix<double, -1, -1, 0, -1, -1> > const&, 
-	//		int, 
-	//		Eigen::PlainObjectBase<Eigen::Matrix<double, -1, 1, 0, -1, 1> >&);
+	template <
+		typename DerivedV,
+		typename DerivedF,
+		typename DerivedP,
+		typename DerivedN,
+		typename DerivedSampl,
+		typename DerivedS,
+		typename DerivedAOVect>
+		IGL_INLINE  bool ambient_occlusion_expanded_embree_sector(
+			const Eigen::PlainObjectBase<DerivedV> & V,
+			const Eigen::PlainObjectBase<DerivedF> & F,
+			const Eigen::PlainObjectBase<DerivedP> & P,
+			const Eigen::PlainObjectBase<DerivedN> & N,
+			const Eigen::PlainObjectBase<DerivedSampl> & samples,
+			Eigen::PlainObjectBase<DerivedS> & S,
+			Eigen::PlainObjectBase< DerivedAOVect> &VO);
+
+	bool face_baricentre(Eigen::MatrixXd &F_baricentres, const Eigen::MatrixXd &V, const Eigen::MatrixXi &F);
+
+	template <
+		typename DerivedP,
+		typename DerivedN,
+		typename DerivedS,
+		typename DerivedAOVect>
+		IGL_INLINE void ambient_occlusion_expanded_impl(
+			const std::function<
+			bool(
+				const Eigen::Vector3f&,
+				const Eigen::Vector3f&)
+			> & shoot_ray,
+			const Eigen::PlainObjectBase<DerivedP> & P,
+			const Eigen::PlainObjectBase<DerivedN> & N,
+			const int num_samples,
+			Eigen::PlainObjectBase<DerivedS> & S,
+			Eigen::PlainObjectBase<DerivedAOVect> & VO)
+	{
+		using namespace Eigen;
+		const int n = P.rows();
+		// Resize output
+		S.resize(n, 1);
+		VO.resize(n, 3);
+
+		// Embree seems to be parallel when constructing but not when tracing rays
+		const MatrixXf D = igl::random_dir_stratified(num_samples).cast<float>();
+
+		const auto & inner = [&P, &N, &num_samples, &D, &S, &shoot_ray, &VO](const int p)
+		{
+			const Vector3f origin = P.row(p).template cast<float>();
+			const Vector3f normal = N.row(p).template cast<float>();
+			int num_hits = 0;
+			Vector3f VectNoOcclud(0, 0, 0);
+			for (int s = 0; s < num_samples; s++)
+			{
+				Vector3f d = D.row(s);
+				if (d.dot(normal) < 0)
+					d *= -1;
+				if (shoot_ray(origin, d))
+					num_hits++;
+				else
+					VectNoOcclud = VectNoOcclud + d.normalized();
+			}
+			S(p) = (double)num_hits / (double)num_samples;
+			auto a = (VectNoOcclud.normalized() / (num_samples - num_hits)).cast<RowVector3d>();
+			if (num_samples != num_hits)
+				VO.row(p) = (VectNoOcclud / (num_samples - num_hits)).transpose().normalized().cast<double>();
+		};
+		igl::parallel_for(n, inner, 1000);
+	}
+
+
+
+	template <
+		typename DerivedV,
+		typename DerivedF,
+		typename DerivedP,
+		typename DerivedN,
+		typename DerivedS,
+		typename DerivedAOVect>
+		IGL_INLINE  void ambient_occlusion_expanded_embree(
+			const Eigen::PlainObjectBase<DerivedV> & V,
+			const Eigen::PlainObjectBase<DerivedF> & F,
+			const Eigen::PlainObjectBase<DerivedP> & P,
+			const Eigen::PlainObjectBase<DerivedN> & N,
+			const int num_samples,
+			Eigen::PlainObjectBase<DerivedS> & S,
+			Eigen::PlainObjectBase< DerivedAOVect> &VO)//vector n occlud
+	{
+		using namespace Eigen;
+		igl::embree::EmbreeIntersector ei;
+		ei.init(V.template cast<float>(), F.template cast<int>());
+
+		const auto & shoot_ray = [&ei](
+			const Eigen::Vector3f& s,
+			const Eigen::Vector3f& dir)->bool
+		{
+			igl::Hit hit;
+			const float tnear = 1e-4f;
+			return ei.intersectRay(s, dir, hit, tnear);
+		};
+		return ambient_occlusion_expanded_impl(shoot_ray, P, N, num_samples, S, VO);
+	}
+
+
+
+
+
+	template <
+		typename DerivedP,
+		typename DerivedN,
+		typename DerivedU,
+		typename DerivedSampl,
+		typename DerivedS,
+		typename DerivedAOVect>
+		IGL_INLINE bool ambient_occlusion_expanded_impl_sectorized(
+			const std::function<
+			bool(
+				const Eigen::Vector3f&,
+				const Eigen::Vector3f&)
+			> & shoot_ray,
+			const Eigen::PlainObjectBase<DerivedP> & P,
+			const Eigen::PlainObjectBase<DerivedN> & N,
+			const Eigen::PlainObjectBase<DerivedU> & U,//first line of face
+			const Eigen::PlainObjectBase<DerivedSampl> &samples, //n_samples X (m_sectors x 3)
+			Eigen::PlainObjectBase<DerivedS> & S, //ratio occlussion
+			Eigen::PlainObjectBase<DerivedAOVect> & VO)//non occluded averaged direction
+	{
+		using namespace Eigen;
+		const int n = P.rows();
+
+		int num_samples = samples.rows();
+		if ((samples.cols() % 3) != 0) {
+			cerr << "ambient_occlusion_expanded_impl_sectorized\n\tNumber of columns not a multiple of 3" << endl;
+			return false;
+		}
+		int num_sectors = std::round(static_cast<double>(samples.cols()) / 3.0);
+
+		S.resize(n, num_sectors);
+		VO.resize(n, 3 * num_sectors);
+
+		//Eigen::IOFormat CleanFmt(4, 0, ", ", "\n", "[", "]");
+
+		const auto & inner = [&P, &N, &U, &num_samples, &num_sectors, &samples, &S, &shoot_ray, &VO/*, &CleanFmt*/](const int p)
+		{
+			const Vector3f origin = P.row(p).template cast<float>();
+			const Vector3f normal = N.row(p).template cast<float>();
+			const Vector3f u_iso = U.row(p).template cast<float>();
+
+			int num_hits = 0;
+
+			Vector3f v_prim_iso = normal.cross(u_iso).normalized();
+			Matrix3f RotSampl;
+			RotSampl <<
+				u_iso(0), v_prim_iso(0), normal(0),
+				u_iso(1), v_prim_iso(1), normal(1),
+				u_iso(2), v_prim_iso(2), normal(2);
+
+			Vector3f VectNoOcclud(0, 0, 0);
+
+			//int invert = 0;
+			for (int s = 0; s < num_samples; s++)
+			{
+				for (int sect = 0; sect < num_sectors; sect++) {
+					Vector3f d = RotSampl * samples.block(s, sect * 3, 1, 3).transpose();
+					//if (d.dot(normal) < 0) {
+					//	d *= -1;
+					//	invert++;
+					//}
+					if (shoot_ray(origin, d))
+						num_hits++;
+					else
+						VectNoOcclud = VectNoOcclud + d.normalized();
+				}
+			}
+			//debug
+			//cout << "nits\t" << num_hits << "\t\tninverts\t" << invert << endl << RotSampl.format(CleanFmt) << endl << endl << endl;
+			S(p) = (double)num_hits / (double)num_samples;
+			auto a = (VectNoOcclud.normalized() / (num_samples - num_hits)).cast<RowVector3d>();
+			if (num_samples != num_hits)
+				VO.row(p) = (VectNoOcclud / (num_samples - num_hits)).transpose().normalized().cast<double>();
+		};
+		igl::parallel_for(n, inner, 1000);
+		return true;
+	}
+
+
+
+
+	template <
+		typename DerivedV,
+		typename DerivedF,
+		typename DerivedP,
+		typename DerivedN,
+		typename DerivedU,
+		typename DerivedSampl,
+		typename DerivedS,
+		typename DerivedAOVect>
+		IGL_INLINE  bool ambient_occlusion_expanded_embree_sector(
+			const Eigen::PlainObjectBase<DerivedV> & V,
+			const Eigen::PlainObjectBase<DerivedF> & F,
+			const Eigen::PlainObjectBase<DerivedP> & P,
+			const Eigen::PlainObjectBase<DerivedN> & N,
+			const Eigen::PlainObjectBase<DerivedU> & U,
+			const Eigen::PlainObjectBase<DerivedSampl> & samples,
+			Eigen::PlainObjectBase<DerivedS> & S,
+			Eigen::PlainObjectBase< DerivedAOVect> &VO)
+	{
+		using namespace Eigen;
+		igl::embree::EmbreeIntersector ei;
+		ei.init(V.template cast<float>(), F.template cast<int>());
+
+		const auto & shoot_ray = [&ei](
+			const Eigen::Vector3f& s,
+			const Eigen::Vector3f& dir)->bool
+		{
+			igl::Hit hit;
+			const float tnear = 1e-4f;
+			return ei.intersectRay(s, dir, hit, tnear);
+		};
+		return AmbientOcclusionExpanded::ambient_occlusion_expanded_impl_sectorized(shoot_ray, P, N, U, samples, S, VO);
+	}
+
+
+
+
+
+
+
+	bool face_baricentre(Eigen::MatrixXd &F_baricentres, const Eigen::MatrixXd &V, const Eigen::MatrixXi &F) {
+		int Frows = F.rows();
+		F_baricentres.resize(Frows, 3);
+#pragma omp parallel for if (Frows>10000)
+		for (int i = 0; i < Frows; i++)
+			F_baricentres.row(i) = ((V.row(F(i, 0)) + V.row(F(i, 1)) + V.row(F(i, 2))) / 3).normalized();
+		return true;
+	}
+
+	bool iso_parameters(Eigen::MatrixXd &U_isoparam, const Eigen::MatrixXd &V, const Eigen::MatrixXi &F) {
+		int Frows = F.rows();
+		U_isoparam.resize(Frows, 3);
+#pragma omp parallel for if (Frows>10000)
+		for (int i = 0; i < Frows; i++)
+			U_isoparam.row(i) = (V.row(F(i, 0)) - V.row(F(i, 2))).normalized();//P1 - P3 // counter-clockwise
+		return true;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
 };
--- PruebasLibiGL/MAIN.cpp
+++ PruebasLibiGL/MAIN.cpp
@@ -15,10 +15,11 @@ Eigen::MatrixXi F;
 Eigen::VectorXd AO;//ratio no occlud
 Eigen::MatrixXd VO;//vector no occlud
 
-
 Eigen::MatrixXd V_face; //baricentre triangles
 Eigen::VectorXd AO_face;
 Eigen::MatrixXd VO_face;
+Eigen::VectorXd AO_face_sect;
+Eigen::MatrixXd VO_face_sect;
 
 // It allows to change the degree of the field when a number is pressed
 bool key_down(igl::opengl::glfw::Viewer& viewer, unsigned char key, int modifier)
@@ -49,6 +50,14 @@ bool key_down(igl::opengl::glfw::Viewer& viewer, unsigned char key, int modifier
 		viewer.data().set_colors(C);
 		break;
 	}
+	case '4':
+	{
+		MatrixXd C = color.replicate(V_face.rows(), 1);
+		for (unsigned i = 0; i < C.rows(); ++i)
+			C.row(i) *= AO_face_sect(i);//std::min<double>(AO(i)+0.2,1);
+		viewer.data().set_colors(C);
+		break;
+	}
 	case '.':
 		viewer.core().lighting_factor += 0.1;
 		break;
@@ -64,15 +73,6 @@ bool key_down(igl::opengl::glfw::Viewer& viewer, unsigned char key, int modifier
 }
 
 
-bool face_baricentre(Eigen::MatrixXd &F_baricentres, const Eigen::MatrixXd &V, const Eigen::MatrixXi &F) {
-	int Frows = F.rows();
-	F_baricentres.resize(Frows, 3);
-#pragma omp parallel for if (Frows>10000)
-	for (int i = 0; i < Frows; i++)
-		F_baricentres.row(i) = ((V.row(F(i, 0)) + V.row(F(i, 1)) + V.row(F(i, 2))) / 3).normalized();
-	return true;
-}
-
 int main(int argc, char *argv[])
 {
 	using namespace std;
@@ -81,22 +81,46 @@ int main(int argc, char *argv[])
 		"Press 1 to turn off Ambient Occlusion" << endl <<
 		"Press 2 to turn on Ambient Occlusion per vertex" << endl <<
 		"Press 3 to turn on Ambient Occlusion per face" << endl <<
+		"Press 4 to turn on Ambient Occlusion per face sectorized" << endl <<
 		"Press . to turn up lighting" << endl <<
 		"Press , to turn down lighting" << endl;
 
-	// Load a mesh in OFF format
 	bool res = igl::readOFF("Recursos/fertility.off", V, F);
 	cout << "V.rows()\t" << V.rows() << endl << "F.rows()\t" << F.rows() << endl;
+
+	//Per vertex
 	MatrixXd N;
 	igl::per_vertex_normals(V, F, N);
 	AmbientOcclusionExpanded::ambient_occlusion_expanded_embree(V, F, V, N, 100, AO, VO);
 	AO = 1.0 - AO.array();
 
+	//Per faces
 	MatrixXd PFN;
 	igl::per_face_normals(V, F, PFN);
-	face_baricentre(V_face, V, F);
+	AmbientOcclusionExpanded::face_baricentre(V_face, V, F);
 	AmbientOcclusionExpanded::ambient_occlusion_expanded_embree(V_face, F, V_face, PFN, 100, AO_face, VO_face);
 	AO_face = 1.0 - AO_face.array();
+
+	//Per faces sectorized
+	//	Rejection sampling
+	int n_azimuth = 2, m_zenith = 2;
+	int n_samples_sector = 100;
+	int tot_samples = n_samples_sector * n_azimuth * m_zenith;
+	MatrixXf samples = igl::random_dir_stratified(tot_samples).cast<float>();
+	//		Upper hemisphere rejection
+	for (int i = 0; i < 100; i++) {
+		Vector3f sampl = samples.block(i, 0, 1, 3).transpose();
+		if (sampl.dot(Vector3f::UnitZ()) < 0)
+			samples.block(i, 0, 1, 3) *= -1;
+	}
+	////		Sector rejection
+	//std::vector<RowVector3f>
+	//for
+	MatrixXd U_iso;
+	AmbientOcclusionExpanded::iso_parameters(U_iso, V, F);
+	AmbientOcclusionExpanded::ambient_occlusion_expanded_embree_sector(V_face, F, V_face, PFN, U_iso, samples, AO_face_sect, VO_face_sect);
+	AO_face_sect = 1.0 - AO_face_sect.array();
+
 
 	//Eigen::IOFormat CleanFmt(4, 0, ", ", "\n", "[", "]");
 	//std::cout << VO.format(CleanFmt);
@@ -105,7 +129,7 @@ int main(int argc, char *argv[])
 	igl::opengl::glfw::Viewer viewer;
 	viewer.data().set_mesh(V, F);
 	viewer.callback_key_down = &key_down;
-	key_down(viewer, '2', 0);
+	key_down(viewer, '4', 0);
 	viewer.data().show_lines = false;
 	viewer.core().lighting_factor = 0.0f;
 	viewer.launch();
